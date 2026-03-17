878c77a195bc8fd0c7e254db53a2b3801f72a651
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-17 22:36:33 +0600

    feat(auth): add user authentication models and setup
    - Added User, Session, Account, and Verification models to the Prisma schema.
    - Implemented database migrations for the new authentication tables.
    - Integrated better-auth for user authentication with email and password support.
    - Updated app to include authentication middleware.

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,12 @@
 import express, { Application } from "express";
 import { postRouter } from "./modules/post/post.routes";
+import { toNodeHandler } from "better-auth/node";
+import { auth } from "./lib/auth";
 
 const app: Application = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.all("/api/auth/*splat", toNodeHandler(auth));
 
 app.get("/", (req, res) => {
     res.send("Welcome to the Prisma Blog App API!");
