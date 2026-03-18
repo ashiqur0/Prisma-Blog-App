8760563d9577a85f61b4b214fa8d2573e70d6a88
ashiqur0 <ashiqurrahmans2018@gmail.com>
2026-03-18 16:16:03 +0600

    feat(auth): integrate nodemailer for email verification and enhance authentication settings

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,17 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { prisma } from "./prisma";
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // Use true for port 465, false for port 587
+    auth: {
+        user: process.env.APP_USER,
+        pass: process.env.APP_PASSWORD
+    },
+});
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -27,5 +38,20 @@ export const auth = betterAuth({
     },
     emailAndPassword: {
         enabled: true,
+        autoSignIn: false,
+        requireEmailVerification: true
+    },
+    emailVerification: {
+        sendVerificationEmail: async ({ user, url, token }, request) => {
+            const info = await transporter.sendMail({
+                from: '"Prisma Blog" <prismablog@gmail.com>',
+                to: "20softstore19@gmail.com",
+                subject: "Hello ✔",
+                text: "Hello world?", // Plain-text version of the message
+                html: "<b>Hello world?</b>", // HTML version of the message
+            });
+
+            console.log("Message sent:", info.messageId);
+        },
     },
 });
